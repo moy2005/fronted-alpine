7a167f1c743c5008c82e0e31933ed956283627d3
dianasanjuan0017 <dianasanjuan0017@gmail.com>
2025-04-02 18:12:03 -0600

    cambio

--- src/pages/autenticacion/ResetPassword.jsx
+++ src/pages/autenticacion/ResetPassword.jsx
@@ -75,7 +75,7 @@ const ResetPassword = () => {
         }
 
         try {
-            const response = await fetch('http://localhost:4000/api/reset-password', {
+            const response = await fetch('https://alpine-gear.vercel.app/api/reset-password', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ token, newPassword: password }),
