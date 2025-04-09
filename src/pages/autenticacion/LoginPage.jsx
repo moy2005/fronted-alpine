ae1a5442439723c1c6193293007e9238ab2c3880
dianasanjuan0017 <dianasanjuan0017@gmail.com>
2025-04-09 12:39:30 -0600

    c

--- src/pages/autenticacion/LoginPage.jsx
+++ src/pages/autenticacion/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useAuth } from "../../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../../styles/login.css";
